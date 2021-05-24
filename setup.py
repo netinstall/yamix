3c83a3da4a78a70b998a4eb0c5d4a34e485ed276
Petr Burmistrov <netinstall@yandex.ru>
2021-05-24 19:13:22 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 
 setup(
-    name='yamix',
-    version='0.1',
-    description='Mix Yandex Music Playlist',
+    name="yamix",
+    version="0.1",
+    scripts=["web.py"]
+    description="Mix Yandex Music Playlist",
     install_requires=[]
 ) 
