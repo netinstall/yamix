8fdb9c1780dd0fc0f7e9dc8111a24445874ac3dc
Petr Burmistrov <netinstall@yandex.ru>
2021-05-24 19:56:44 +0300

    add deps

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 
 setup(
     name="yamix",
-    version="0.1.5",
+    version="0.1.6",
     scripts=["web.py"],
     description="Mix Yandex Music Playlist",
-    install_requires=[],
-    packages=['yamix']
+    install_requires=[
+        "bottle",
+        "requests"],
+    packages=["yamix"]
 ) 
