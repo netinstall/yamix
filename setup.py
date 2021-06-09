e3545cca1879ba2a23ae5c1b74ad781eb74af676
Petr Burmistrov <netinstall@yandex.ru>
2021-06-10 01:10:10 +0300

    change shebang

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="yamix",
-    version="0.1.6",
+    version="0.1.7",
     scripts=["web.py"],
     description="Mix Yandex Music Playlist",
     install_requires=[
